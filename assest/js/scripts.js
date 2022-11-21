86e5a175ed343cac9c855b22b8ee9d82c9796939
bashar Al-Daher <basharaldaher011@gmail.com>
2022-11-21 15:35:29 +0200

    add contact section

--- assest/js/scripts.js
+++ assest/js/scripts.js
@@ -6,3 +6,18 @@ function showMobileNav() {
         topNav.className = "top-nav";
     }
   }
+
+  var toTopArrow = document.getElementsByClassName("to-top");
+// toTopArrow[0].style.display='none'
+// console.log(window.innerHeight)
+window.onscroll = function(){
+    let value = scrollY;
+    // console.log(value)
+    if(value>=800){
+        toTopArrow[0].style.bottom='0px'
+    }
+    else{
+        toTopArrow[0].style.bottom='-100px'
+    }
+}
+
